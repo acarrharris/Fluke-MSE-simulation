62739f713ebf1d1f1e00283f561219c627b9af49
Andrew Carr-Harris <andrew.carr-harris@noaa.gov>
2021-12-27 10:37:05 -0500

    Adjusted mean and variance of catch per trip in pred. year
    I added code that solves for the neg binomial size parameter in the prediction year such that the CV for catch-per-trip in the prediction year equals CV in the baseline year. The new mu and size parameters, for each of the 3 regions, are incorporated into the mvdc() function.

--- predicted_catch_per_trip.R
+++ predicted_catch_per_trip.R
@@ -1,4 +1,4 @@
-#The following creates an cathc-per-trip dataset adjusted to reflect the population size
+#The following creates an catch-per-trip dataset adjusted to reflect the population size
 
 ###Northern states
 #catch_data <- read_excel("observed_catch_NO_19.xlsx")
@@ -10,24 +10,53 @@ bsb <- catch_data$bsb_tot_cat
 
 #estimate the nb parameters
 nbfit_sf = fitdistr(sf, "Negative Binomial")
-nbfit_sf
+summary(nbfit_sf)
 
 sf_mu <- nbfit_sf$estimate['mu']
-sf_mu
-
-#Here, scale sf_mu by the scale factor 
-sf_mu=sf_mu*catch_expansion_factor_NO
 sf_mu
 
 sf_size <- nbfit_sf$estimate['size']
 sf_size
 
+
+# We now adjust mean catch per trip by the expansion factor. 
+# We will assume that in the prediction year under a new mean catch per trip, 
+# the SD relative to the mean remains as it was in the baseline year. 
+# We therefore adjust the 'size' parameter for the prediction year distribution such that: coef. var. prediction year = Coef. var. 2019  
+
+# From ?NegBinomial:
+# "An alternative parametrization (often used in ecology) is by the mean mu (see above), 
+# and size, the dispersion parameter, where prob = size/(size+mu). The variance is mu + mu^2/size 
+# in this parametrization." 
+
+#calculate variance and CV in baseline year
+var_sf=sf_mu+(sf_mu^2)/sf_size
+cv_sf_base = sqrt(var_sf)/sf_mu
+cv_sf_base
+
+# Now adjust the mu parameter for prediction year distribution by the expansion factor
+sf_mu_new=sf_mu*catch_expansion_factor_NO
+
+#solve for new size parameter 
+sf_size_new=(sf_mu_new^2)/(((cv_sf_base*sf_mu_new)^2)-sf_mu_new)
+
+#new variance and CV
+var_sf_new=sf_mu_new+(sf_mu_new^2)/sf_size_new
+cv_sf_new = sqrt(var_sf_new)/sf_mu_new
+
+#Check that CV old and CV new are the same
+cv_sf_base
+cv_sf_new
+
+
+# Get the black sea bass parameters 
 nbfit_bsb <- fitdistr(bsb, "Negative Binomial")
 nbfit_bsb
 
 bsb_mu <- nbfit_bsb$estimate['mu']
 bsb_mu
 bsb_size <- nbfit_bsb$estimate['size']
+bsb_size
 bsb_size
 
 
@@ -45,7 +74,7 @@ df <- coef(fit)[2]
 
 
 t_copula_nb <- mvdc(copula=tCopula(rho,dim=2,df=df),  margins = c("nbinom","nbinom"), 
-                    paramMargins=list(list(mu=sf_mu, size=sf_size),
+                    paramMargins=list(list(mu=sf_mu_new, size=sf_size_new),
                                       list(mu=bsb_mu, size=bsb_size)))
 
 sim_t_cop_nb <- rMvdc(30000, t_copula_nb )
@@ -71,24 +100,53 @@ bsb <- catch_data$bsb_tot_cat
 
 #estimate the nb parameters
 nbfit_sf = fitdistr(sf, "Negative Binomial")
-nbfit_sf
+summary(nbfit_sf)
 
 sf_mu <- nbfit_sf$estimate['mu']
-sf_mu
-
-#Here, scale sf_mu by the scale factor 
-sf_mu=sf_mu*catch_expansion_factor_NJ
 sf_mu
 
 sf_size <- nbfit_sf$estimate['size']
 sf_size
 
+
+# We now adjust mean catch per trip by the expansion factor. 
+# We will assume that in the prediction year under a new mean catch per trip, 
+# the SD relative to the mean remains as it was in the baseline year. 
+# We therefore adjust the 'size' parameter for the prediction year distribution such that: coef. var. prediction year = Coef. var. 2019  
+
+# From ?NegBinomial:
+# "An alternative parametrization (often used in ecology) is by the mean mu (see above), 
+# and size, the dispersion parameter, where prob = size/(size+mu). The variance is mu + mu^2/size 
+# in this parametrization." 
+
+#calculate variance and CV in baseline year
+var_sf=sf_mu+(sf_mu^2)/sf_size
+cv_sf_base = sqrt(var_sf)/sf_mu
+cv_sf_base
+
+# Now adjust the mu parameter for prediction year distribution by the expansion factor
+sf_mu_new=sf_mu*catch_expansion_factor_NJ
+
+#solve for new size parameter 
+sf_size_new=(sf_mu_new^2)/(((cv_sf_base*sf_mu_new)^2)-sf_mu_new)
+
+#new variance and CV
+var_sf_new=sf_mu_new+(sf_mu_new^2)/sf_size_new
+cv_sf_new = sqrt(var_sf_new)/sf_mu_new
+
+#Check that CV old and CV new are the same
+cv_sf_base
+cv_sf_new
+
+
+# Get the black sea bass parameters 
 nbfit_bsb <- fitdistr(bsb, "Negative Binomial")
 nbfit_bsb
 
 bsb_mu <- nbfit_bsb$estimate['mu']
 bsb_mu
 bsb_size <- nbfit_bsb$estimate['size']
+bsb_size
 bsb_size
 
 
@@ -107,7 +165,7 @@ df <- coef(fit)[2]
 
 
 t_copula_nb <- mvdc(copula=tCopula(rho,dim=2,df=df),  margins = c("nbinom","nbinom"), 
-                    paramMargins=list(list(mu=sf_mu, size=sf_size),
+                    paramMargins=list(list(mu=sf_mu_new, size=sf_size_new),
                                       list(mu=bsb_mu, size=bsb_size)))
 
 sim_t_cop_nb <- rMvdc(30000, t_copula_nb )
@@ -133,24 +191,53 @@ bsb <- catch_data$bsb_tot_cat
 
 #estimate the nb parameters
 nbfit_sf = fitdistr(sf, "Negative Binomial")
-nbfit_sf
+summary(nbfit_sf)
 
 sf_mu <- nbfit_sf$estimate['mu']
-sf_mu
-
-#Here, scale sf_mu by the scale factor 
-sf_mu=sf_mu*catch_expansion_factor_SO
 sf_mu
 
 sf_size <- nbfit_sf$estimate['size']
 sf_size
 
+
+# We now adjust mean catch per trip by the expansion factor. 
+# We will assume that in the prediction year under a new mean catch per trip, 
+# the SD relative to the mean remains as it was in the baseline year. 
+# We therefore adjust the 'size' parameter for the prediction year distribution such that: coef. var. prediction year = Coef. var. 2019  
+
+# From ?NegBinomial:
+# "An alternative parametrization (often used in ecology) is by the mean mu (see above), 
+# and size, the dispersion parameter, where prob = size/(size+mu). The variance is mu + mu^2/size 
+# in this parametrization." 
+
+#calculate variance and CV in baseline year
+var_sf=sf_mu+(sf_mu^2)/sf_size
+cv_sf_base = sqrt(var_sf)/sf_mu
+cv_sf_base
+
+# Now adjust the mu parameter for prediction year distribution by the expansion factor
+sf_mu_new=sf_mu*catch_expansion_factor_SO
+
+#solve for new size parameter 
+sf_size_new=(sf_mu_new^2)/(((cv_sf_base*sf_mu_new)^2)-sf_mu_new)
+
+#new variance and CV
+var_sf_new=sf_mu_new+(sf_mu_new^2)/sf_size_new
+cv_sf_new = sqrt(var_sf_new)/sf_mu_new
+
+#Check that CV old and CV new are the same
+cv_sf_base
+cv_sf_new
+
+
+# Get the black sea bass parameters 
 nbfit_bsb <- fitdistr(bsb, "Negative Binomial")
 nbfit_bsb
 
 bsb_mu <- nbfit_bsb$estimate['mu']
 bsb_mu
 bsb_size <- nbfit_bsb$estimate['size']
+bsb_size
 bsb_size
 
 
@@ -169,7 +256,7 @@ df <- coef(fit)[2]
 
 
 t_copula_nb <- mvdc(copula=tCopula(rho,dim=2,df=df),  margins = c("nbinom","nbinom"), 
-                    paramMargins=list(list(mu=sf_mu, size=sf_size),
+                    paramMargins=list(list(mu=sf_mu_new, size=sf_size_new),
                                       list(mu=bsb_mu, size=bsb_size)))
 
 sim_t_cop_nb <- rMvdc(30000, t_copula_nb )
